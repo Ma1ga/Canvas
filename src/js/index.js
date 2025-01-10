290d7b0c6f99960359eb4d578ae47ae0e75891e1
Matvii <kondratukmatvyj@gmail.com>
2025-01-10 17:54:01 +0200

    fourth

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,12 @@ const size90x150 = document.getElementById("size90x150");
 const buttonSizeOption = document.getElementById("button-size");
 const someBuy = document.querySelector(".total-price");
 const extraOptions = document.querySelectorAll(".extraInput");
+const customSizeX = document.getElementById("width-custom-size");
+const customSizeY = document.getElementById("height-custom-size");
+const customSizeButton = document.getElementById("button-custom-size");
 
 let basePrice = 0; 
 let extraPrice = 0; 
-
 
 function checkSize() {
   if (size30x30.checked) {
@@ -24,6 +26,25 @@ function checkSize() {
 }
 
 
+function checkCustomSize() {
+  const width = parseInt(customSizeX.value);
+  const height = parseInt(customSizeY.value);
+
+  if (!width || !height) {
+    alert("Введите коррекктные размеры");
+    return;
+  }
+
+  if (width < 30 || width > 90 || height < 30 || height > 150) {
+    alert("Размер должен быть в пределах от 30 до 90 по ширине и от 30 до 150 по высоте");
+    return;
+  }
+
+  basePrice = (width * height) * 0.04; 
+  updateTotalPrice();
+}
+
+
 function updateExtraPrice() {
   extraPrice = 0; 
   extraOptions.forEach((option) => {
@@ -33,16 +54,15 @@ function updateExtraPrice() {
   });
 }
 
-
+// Обновляем итоговую стоимость
 function updateTotalPrice() {
   updateExtraPrice(); 
   someBuy.innerHTML = basePrice + extraPrice + " грн"; 
 }
 
-
+// Обработчики событий
 buttonSizeOption.onclick = checkSize;
-
-
+customSizeButton.onclick = checkCustomSize;
 extraOptions.forEach((option) => {
   option.onchange = updateTotalPrice;
 });
