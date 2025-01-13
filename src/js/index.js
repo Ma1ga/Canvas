9d7c16c93402fb31fdf67b64e0769132c2a93cc8
Matvii <kondratukmatvyj@gmail.com>
2025-01-13 17:56:24 +0200

    six

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,14 @@ const size30x30 = document.getElementById("size30x30");
 const size30x90 = document.getElementById("size30x90");
 const size90x120 = document.getElementById("size90x120");
 const size90x150 = document.getElementById("size90x150");
-const buttonSizeOption = document.getElementById("button-size");
 const someBuy = document.querySelector(".total-price");
 const extraOptions = document.querySelectorAll(".extraInput");
 const customSizeX = document.getElementById("width-custom-size");
 const customSizeY = document.getElementById("height-custom-size");
 const customSizeButton = document.getElementById("button-custom-size");
-const canvasBox = document.querySelector(".canvas-box");
 const canvasWrapper = document.querySelector(".canvas-wrapper");
-
+const customSizeWrapper = document.querySelector(".custom-size-wrapper")
+const buttonToggle = document.getElementById("toggle-custom")
 let basePrice = 0; 
 let extraPrice = 0; 
 
@@ -18,9 +17,6 @@ function updateCanvasSize(width, height) {
   canvasWrapper.style.width = (width * 3) + "px";
   canvasWrapper.style.height = (height * 3) + "px";
 }
-
-
-
 
 function checkSize() {
   if (size30x30.checked) {
@@ -34,7 +30,7 @@ function checkSize() {
     updateCanvasSize(90, 120);
   } else if (size90x150.checked) {
     basePrice = 345;
-    updateCanvasSize(90, 120);
+    updateCanvasSize(90, 150);
   }
   updateTotalPrice(); 
 }
@@ -54,7 +50,6 @@ function checkCustomSize() {
     return;
   }
 
-  
   if (width === 30 && height === 30) {
     basePrice = 50;
   } else if (width === 30 && height === 90) {
@@ -64,15 +59,14 @@ function checkCustomSize() {
   } else if (width === 90 && height === 150) {
     basePrice = 345;
   } else {
-    
     basePrice = (width * height) * 0.04;
   }
   updateCanvasSize(width, height);
   updateTotalPrice();
 }
-
-
-
+function ShowCustom() {
+  buttonToggle.onclick = customSizeWrapper
+}
 function updateExtraPrice() {
   extraPrice = 0; 
   extraOptions.forEach((option) => {
@@ -82,15 +76,20 @@ function updateExtraPrice() {
   });
 }
 
-
 function updateTotalPrice() {
   updateExtraPrice(); 
   someBuy.innerHTML = basePrice + extraPrice + " грн"; 
 }
 
-// Обработчики событий
-buttonSizeOption.onclick = checkSize;
+
+size30x30.onchange = checkSize;
+size30x90.onchange = checkSize;
+size90x120.onchange = checkSize;
+size90x150.onchange = checkSize;
+
 customSizeButton.onclick = checkCustomSize;
+
+
 extraOptions.forEach((option) => {
   option.onchange = updateTotalPrice;
 });
